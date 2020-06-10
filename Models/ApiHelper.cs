47723d30f89dc2ed2e4c741487ac3011fec75312
mai cook <mailiecook@yahoo.com>
2020-06-10 15:50:12 -0700

    debug post method, ablility to post to books list and see details

--- Models/ApiHelper.cs
+++ Models/ApiHelper.cs
@@ -1,5 +1,6 @@
 using System.Threading.Tasks;
 using RestSharp;
+using System;
 
 namespace bookstore.Models
 {
@@ -8,7 +9,7 @@ namespace bookstore.Models
     public static async Task<string> GetAll()
     {
       RestClient client = new RestClient("http://localhost:5006/api");
-      RestRequest request = new RestRequest($"Books", Method.GET);
+      RestRequest request = new RestRequest($"books", Method.GET);
       var response = await client.ExecuteTaskAsync(request);
       return response.Content;
     }
@@ -16,22 +17,23 @@ namespace bookstore.Models
     public static async Task<string> Get(int id)
     {
       RestClient client = new RestClient("http://localhost:5006/api");
-      RestRequest request = new RestRequest($"Books/{id}", Method.GET);
+      RestRequest request = new RestRequest($"books/{id}", Method.GET);
       var response = await client.ExecuteTaskAsync(request);
       return response.Content;
     }
 
     public static async Task Post(string newBook)
     {
-      RestClient client = new RestClient("http://localhost:5000/api");
+      RestClient client = new RestClient("http://localhost:5006/api");
       RestRequest request = new RestRequest($"books", Method.POST);
       request.AddHeader("Content-Type", "application/json");
+      Console.WriteLine(newBook);
       request.AddJsonBody(newBook);
       var response = await client.ExecuteTaskAsync(request);
     }
     public static async Task Put(int id, string newBook)
     {
-      RestClient client = new RestClient("http://localhost:5000/api");
+      RestClient client = new RestClient("http://localhost:5006/api");
       RestRequest request = new RestRequest($"books/{id}", Method.PUT);
       request.AddHeader("Content-Type", "application/json");
       request.AddJsonBody(newBook);
@@ -39,7 +41,7 @@ namespace bookstore.Models
     }
     public static async Task Delete(int id)
     {
-      RestClient client = new RestClient("http://localhost:5000/api");
+      RestClient client = new RestClient("http://localhost:5006/api");
       RestRequest request = new RestRequest($"books/{id}", Method.DELETE);
       request.AddHeader("Content-Type", "appliction/json");
       var response = await client.ExecuteTaskAsync(request);
